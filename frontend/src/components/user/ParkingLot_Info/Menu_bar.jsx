f3901d1f01fc0b6657d07c6f34328a69dfc24621
quanganh6905 <160844203+quanganh6905@users.noreply.github.com>
2025-05-25 15:40:05 +0700

    update

--- frontend/src/components/user/ParkingLot_Info/Menu_bar.jsx
+++ frontend/src/components/user/ParkingLot_Info/Menu_bar.jsx
@@ -5,25 +5,33 @@ import { Route, Routes } from "react-router-dom";
 import ParkingRevenue from "./ParkingRevenue";
 import ParkingLot_Info from "./ParkingLot_Info";
 import UserInfo from "./user_Info";
+
 const Menu_bar = () => {
   return (
     <div className={styles.menuBar}>
       <div className={styles["menu-bar-right"]}>
         <NavLink
           to="/my-parking"
-          className={({ isActive }) => `${styles["nav-link"]} `}
+          end // Thêm end prop để chỉ active khi đúng path
+          className={({ isActive }) => 
+            `${styles["nav-link"]} ${isActive ? styles.active : ''}`
+          }
         >
           Bãi đỗ của tôi
         </NavLink>
         <NavLink
           to="/my-parking/renue"
-          className={({ isActive }) => `${styles["nav-link"]} `}
+          className={({ isActive }) => 
+            `${styles["nav-link"]} ${isActive ? styles.active : ''}`
+          }
         >
-          Xem doanh thu
+          Thống kê
         </NavLink>
         <NavLink
           to="/my-parking/my-profile"
-          className={({ isActive }) => `${styles["nav-link"]} `}
+          className={({ isActive }) => 
+            `${styles["nav-link"]} ${isActive ? styles.active : ''}`
+          }
         >
           Thông tin cá nhân
         </NavLink>
