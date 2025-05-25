22764fc49845bda2e866fa814930253dc3fafde6
XyzHuy <149671889+XyzHuy@users.noreply.github.com>
2025-05-25 08:45:00 +0700

    Userlist duplicate error fix

--- frontend/src/views/App.jsx
+++ frontend/src/views/App.jsx
@@ -15,7 +15,6 @@ import UserList from "../components/Admin/UserList";
 import ParkingList from "../components/Admin/ParkingList";
 import { getUserName } from "../controllers/App_Controller";
 import History from "../components/user/querryHistory/History";
-import UserList from "../components/Admin/UserList";
 import "../styles/App.css";
 
 function App() {
